7606290652c6412d2b60fd192e51740c12749a82
Indrajeet Kumar <ikumar@planittesting.com>
2018-12-10 21:29:50 +1100

    commiting demo project

---
+++ src/com/palnit/testsetup/TestBase.java
@@ -0,0 +1,30 @@
+package com.palnit.testsetup;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestBase {
+
+	protected WebDriver driver;
+
+	@BeforeMethod
+	public void setUp() {
+		System.setProperty("webdriver.chrome.exe", "chromedriver.exe");
+		driver = new ChromeDriver();
+
+	}
+
+	@Test
+	public void launchApplication() {
+		driver.get("http://www.google.com");
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
